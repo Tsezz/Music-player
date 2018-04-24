206d99efc277cfe9359cf006533e4c86273f2a18
Tsezz <xlaptx@gmail.com>
2018-04-24 16:09:47 +0800

    onclick active song-list

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -12,7 +12,7 @@
             let {songs} = data
             let liList = songs.map((song)=>{
                 let li = $('<li></li>')
-                li.text(song.name)
+                li.text(song.name).attr('data-song-id',song.id)
                 return li
             })            
             $el.find('ul').empty()
@@ -20,6 +20,11 @@
                 $el.find('ul').append(domLi)
             })
             // $(this.el).html(this.template)
+        },
+        activeItem(li){
+            let $li = $(li)
+            $li.addClass('active')
+                .siblings('.active').removeClass('active')
         },
         clearActive(){
             $(this.el).find('.active').removeClass('actvie')
@@ -48,14 +53,28 @@
             this.view = view
             this.model = model
             this.view.render(this.model.data)
+            this.bindEvents()
+            this.bindEventHub()
+            this.getAllSongs()
+        },
+        getAllSongs(){
+            return this.model.find().then(()=>{
+                this.view.render(this.model.data)
+            })
+        },
+        bindEvents(){
+            $(this.view.el).on('click','li',(e)=>{
+                this.view.activeItem(e.currentTarget)
+                let songId = e.currentTarget.getAttribute('data-song-id')
+                window.eventHub.emit('select',{id:songId})
+            })
+        },
+        bindEventHub(){
             window.eventHub.on('upload',()=>{
                 this.view.clearActive()
             })
             window.eventHub.on('create', (songData)=>{
                 this.model.data.songs.push(songData)
-                this.view.render(this.model.data)
-            })
-            this.model.find().then(()=>{
                 this.view.render(this.model.data)
             })
         }
